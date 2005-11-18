e1b2e5bc51ee956cc741a5eda2adbf6fd9fb9b94
Nik <nik_89@Datagram_4.(none)>
2005-11-17 23:37:14 -0500

    method to make each modules have access to the other modules

---
+++ include/extlib.h
@@ -0,0 +1,52 @@
+/* Library Abstraction Layer header */
+
+#ifndef __EXTLIB_H
+#define __EXTLIB_H
+
+#include <neuro_engine.h>
+
+extern int Lib_VideoInit(v_object **screen, v_object **screen_buf);
+
+extern void Lib_VideoExit();
+
+extern int Lib_EventInit();
+
+extern void Lib_EventExit();
+
+
+extern void Lib_LockVObject(v_object *vobj);
+
+extern void Lib_UnlockVObject(v_object *vobj);
+
+extern void Lib_FreeVobject(v_object *source);
+
+extern void Lib_Flip(v_object *source);
+
+extern void Lib_FillRect(v_object *source, Rectan *src, u32 color);
+
+extern void Lib_UpdateRect(v_object *source, Rectan *src);
+
+extern void Lib_BlitObject(v_object *source, Rectan *src, v_object *destination, Rectan *dst);
+
+extern void Lib_GiveVobjectProp(v_object *source, Rectan *output);
+
+extern void Lib_GetVObjectData(v_object *vobj, u32 *flags, i32 *h, i32 *w, u32 *pitch, void **pixels, Rectan **clip_rect, u8 *bpp, u32 *Rmask, u32 *Gmask, u32 *Bmask, u32 *Amask);
+
+extern v_object *Lib_LoadBMP(const char *path);
+
+extern u32 Lib_MapRGB(v_object *vobj, u8 r, u8 g, u8 b);
+
+extern void Lib_SetColorKey(v_object *vobj, u32 flag,u32 key);
+
+extern v_object * Lib_CreateVObject(u32 flags, i32 width, i32 height, i32 depth, u32 Rmask, u32 Gmask, u32 Bmask, u32 Amask);
+
+/* a higher SDL function which is kinda an hack for event.c */
+extern void Lib_EventPoll();
+
+extern i32 Lib_PollEvent(void *event);
+
+extern u8 *Lib_GetKeyState(i32 *numkeys);
+
+extern u8 Lib_GetMouseState(i32 *x, i32 *y); 
+
+#endif /* not __EXTLIB_H */
---
+++ include/graphics.h
@@ -0,0 +1,109 @@
+/*! \file graphics.h
+ * \brief graphics engine header
+ *
+ * The graphic engine is part of the most important engines in a game. 
+ * It provides function to draw images on the screen.
+ */
+
+#ifndef __GRAPHICS_H
+#define __GRAPHICS_H
+
+#include <neuro_engine.h>
+#include <other.h>
+
+#include <stdlib.h>
+
+/*! default : 60
+ * the maximum amount of frames per seconds -deprecated-
+ */
+#define FRAMES_PER_SECOND 60
+
+/*! default : 10 -- moved to ebuf.h
+ * the amount of memory that we allocate
+ * in addition to the mem required. 
+ * */
+/* #define MEMORY_ALLOC_OVERH 1 */
+
+/*! default : 2
+ * how many time the computer has to check 
+ * out which one of the image has the lowest
+ * layer(to be placed in the instruction
+ * buffer).
+ */
+#define LAYER_CHECK_PER_FRAMES 2
+
+/*! The Total screen width (can be changed before compile time to set the new size) */
+#define SCREEN_X 800
+/*! The Total screen height (can be changed before compile time to set the new size) */
+#define SCREEN_Y 600
+
+extern void *Neuro_GetScreenBuffer();
+
+/*! Add a Drawing Instruction to the graphics loop. This function will
+ * put take the image, place it in a special buffer and then draw the image
+ * with the desired draw time(in comparison with other to draw images : layer) and 
+ * place it on the screen depending on the given coordinates (also draws the portion
+ * of the image that you set). This function is in general meant to be inside a function
+ * that has a pointer callback set in graphics_AddDrawingElement().
+ * @param [in] layer the drawing order by which the frame needs to be drawn on screen 
+ * @param [in] src the image rectangle to draw (to select different parts of a single image)
+ * @param [in] dst the coordinate where the image must be drawn to.
+ * @param [in] surface the pointer to the image itself.
+ */
+extern void Neuro_AddDrawingInstruction(u8 layer, Rectan *src, Rectan *dst, void *surface);
+
+/*! This function is used to add a callback function in a buffer. This buffer is looped and 
+ * the callback functions(which are added using this function) are ran one by one every cycles. 
+ * @param [in] func this is the callback function that the graphics engine will loop and 
+ * run every cycles 
+ */
+extern void Neuro_AddDrawingElement(void (*func)());
+
+/* directly draw a surface on the screen without
+ * the layering feature.
+ */
+extern void Neuro_AddDirectDrawing(Rectan *src, Rectan *dst, void *surface);
+
+/*! use this function to set the background */
+extern void Neuro_AddBackground(void *isurface);
+
+/*! give the current fps */
+extern void Neuro_GiveFPS(t_tick *output);
+
+/* draw a pixel on the screen */
+extern void Neuro_PutPixel(u32 x, u32 y, u32 pixel);
+
+/* get the color of a pixel on the screen */
+extern u32 Neuro_GetPixel(u32 x, u32 y);
+
+/* clean all the pixels that were drawn using the handled pixel drawer in this module */
+extern void Neuro_CleanPixels();
+
+
+typedef struct ENGINEBUF ENGINEBUF;
+
+extern ENGINEBUF *Neuro_CreateEngineBuf();
+
+extern void Neuro_AllocEngineBuf(ENGINEBUF *eng, size_t sptp, size_t sobj);
+
+extern void Neuro_CleanEngineBuf(ENGINEBUF **eng);
+
+extern u32 Neuro_GiveEngineBufCount(ENGINEBUF *eng);
+
+extern void **Neuro_GiveEngineBufAddr(ENGINEBUF *eng, u32 elem);
+
+extern void *Neuro_GiveEngineBuf(ENGINEBUF *eng, u32 elem);
+
+extern void Neuro_SetEngineBuf(ENGINEBUF *eng, void **to, void *from);
+
+
+/*! internal use Initialisation of the Graphics Engine */
+extern int Graphics_Init(void);
+/*! Poll of the Graphics Engine */
+extern void Graphics_Poll(void);
+/*! Cleaning of the Graphics Engine */
+extern void Graphics_Clean(void);
+
+
+
+#endif /* __GRAPHICS_H */
