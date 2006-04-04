de8eb8cd0becaea7d4de6ad113fddc13d26f8c49
Nik <nik_89@nik.datagram.homelinux.com>
2006-04-04 01:33:43 -0400

    added the legal header to all the source and header files.

--- src/xml/xmltool.c
+++ src/xml/xmltool.c
@@ -1,4 +1,25 @@
 
+/*    
+ * libneuro, a light weight abstraction of high or lower libraries 
+ * and toolkit for applications.
+ * Copyright (C) 2005-2006  Nicholas Niro, Robert Lemay
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
 /*!
  * xmltool.c
  * the basic xml functions for the project xmltools are here
@@ -21,8 +42,8 @@
 
 /*--- local ---*/
 
-#include "xmltool.h"
-#include "nmap.h"
+#include <xmltool.h>
+#include <nmap.h>
 
 
 /*--- static variables ---*/
