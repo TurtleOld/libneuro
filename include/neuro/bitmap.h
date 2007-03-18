e13d405e2aea3dea897048b4ec9b55f5c1b225a7
Nicholas Niro <nik_89@neuroponic.com>
2007-03-18 02:52:17 -0500

    moved the file bitmap.h to the normal headers location and is installed. Although the functions shouldn't be accessed directly by external code.

---
+++ include/neuro/bitmap.h
@@ -0,0 +1,13 @@
+/* bitmap.h */
+
+#ifndef __BITMAP_H
+#define __BITMAP_H
+
+typedef struct BMP_CTX BMP_CTX;
+
+extern i8 Bitmap_Poll(BMP_CTX *ctx);
+extern BMP_CTX *Bitmap_CreateCTX(const char *path);
+
+extern v_object *Bitmap_DestroyCTX(BMP_CTX *ctx);
+
+#endif /* NOT __BITMAP_H */
