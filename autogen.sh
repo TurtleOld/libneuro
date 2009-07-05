3f360a3e62cc930362d05b726d721c7d5c6ee3f9
Nicholas Niro <nik_89@neuroponic.com>
2009-07-04 23:37:48 -0400

    Added completeness to the script.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-autoreconf
+autoheader
+
+automake --add-missing
+
+libtoolize
+
+autoreconf -v
+
+echo "	You can now run ./configure"
+echo "	Then compile by doing : make"
+echo "	And then install by doing (as root or otherwise) : make install"
